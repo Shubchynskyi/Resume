775b16e5a7791f7c09113ead2dcbbd5dbdb062db
Shubchynskyi <shubchinskiydmitriy@gmail.com>
2025-02-02 15:15:02 +0100

    - update index.js

--- js/index.js
+++ js/index.js
@@ -26,30 +26,22 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
-    // Get all elements with class 'modal'
     const modals = document.getElementsByClassName('modal');
-    // Get all elements with id starting with 'myBtn'
     const btns = Array.from(document.querySelectorAll('[id^="myBtn"]'));
-    // Get all elements with class 'close'
     const spans = Array.from(document.getElementsByClassName('close'));
 
-    // Iterate over each button
     btns.forEach(function (btn, index) {
-        // Add a click event listener that opens the corresponding modal window
         btn.onclick = function () {
             modals[index].style.display = "block";
         };
     });
 
-    // Iterate over each 'close' element
     spans.forEach(function (span, index) {
-        // Add a click event listener that closes the corresponding modal window
         span.onclick = function () {
             modals[index].style.display = "none";
         };
     });
 
-    // Initialize onclick event for the window to close modal windows when clicked outside
     window.onclick = function (event) {
         Array.from(modals).forEach(function (modal) {
             if (event.target === modal) {
